bce54f26fee8818d6deb37ca3bfc30810f70dbbf
Aniket Kumar Singh <aniket13kumar@gmail.com>
2023-12-29 15:32:03 +0530

    added in the fixes in pom.xml

---
+++ src/main/java/com/example/aniket/jobsearch/JobSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.aniket.jobsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchApplication.class, args);
+	}
+
+}
